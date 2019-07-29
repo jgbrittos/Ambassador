0906518dac012131a1f8493fcd53baa790d3b019
João Gabriel de Britto e Silva <joao.britto@easynvest.com.br>
2019-07-29 09:29:41 -0300

    Fixing similar routes problem considering regex patterns

--- Ambassador/Router.swift
+++ Ambassador/Router.swift
@@ -11,14 +11,14 @@ import Foundation
 /// Router WebApp for routing requests to different WebApp
 open class Router: WebApp {
     var routes: [String: WebApp] = [:]
+
     open var notFoundResponse: WebApp = DataResponse(
         statusCode: 404,
         statusMessage: "Not found"
     )
     private let semaphore = DispatchSemaphore(value: 1)
 
-    public init() {
-    }
+    public init() {}
 
     open subscript(path: String) -> WebApp? {
         get {
@@ -40,12 +40,18 @@ open class Router: WebApp {
         }
     }
 
-    open func app(
-        _ environ: [String: Any],
-        startResponse: @escaping ((String, [(String, String)]) -> Void),
-        sendBody: @escaping ((Data) -> Void)
-    ) {
-        let path = environ["PATH_INFO"] as! String
+    open func app(_ environ: [String: Any],
+                  startResponse: @escaping ((String, [(String, String)]) -> Void),
+                  sendBody: @escaping ((Data) -> Void)) {
+        guard let pathInfo = environ["PATH_INFO"] as? String else {
+            return notFoundResponse.app(environ, startResponse: startResponse, sendBody: sendBody)
+        }
+        
+        var path = pathInfo
+        
+        if let queryString = environ["QUERY_STRING"] as? String {
+            path += "?" + queryString
+        }
 
         if let (webApp, captures) = matchRoute(to: path) {
             var environ = environ
@@ -57,23 +63,34 @@ open class Router: WebApp {
     }
 
     private func matchRoute(to searchPath: String) -> (WebApp, [String])? {
+        var app: [(WebApp, [String])]? = []
+        
         for (path, route) in routes {
             let regex = try! NSRegularExpression(pattern: path, options: [])
-            let matches = regex.matches(
-                in: searchPath,
-                options: [],
-                range: NSRange(location: 0, length: searchPath.count)
-            )
+            let matches = regex.matches(in: searchPath, options: [], range: NSRange(location: 0, length: searchPath.count))
+            
             if !matches.isEmpty {
-                let searchPath = NSString(string: searchPath)
                 let match = matches[0]
-                var captures = [String]()
-                for rangeIdx in 1 ..< match.numberOfRanges {
-                    captures.append(searchPath.substring(with: match.range(at: rangeIdx)))
+                let matchedPath = extract(match, in: searchPath)
+                
+                if !matchedPath.isEmpty, matchedPath == searchPath {
+                    let searchPath = NSString(string: searchPath)
+                    var captures = [String]()
+                    for rangeIdx in 1 ..< match.numberOfRanges {
+                        captures.append(searchPath.substring(with: match.range(at: rangeIdx)))
+                    }
+                    app?.append((route, captures))
                 }
-                return (route, captures)
             }
         }
-        return nil
+        return app?.last
+    }
+    
+    private func extract(_ match: NSTextCheckingResult, in text: String) -> String {
+        if let index = Range(match.range, in: text) {
+            return String(text[index])
+        }
+        
+        return ""
     }
 }
